b3342caf16f60533adc60b17dd34c62e54ea9e14
ks1610 <trinhvinhkhanh1610@gmail.com>
2025-11-05 09:51:25 +0700

    final 4

--- app.py
+++ app.py
@@ -313,7 +313,7 @@ if uploaded_file is not None:
                     "Non-Phone": ("⚠️ Sản phẩm không phải là điện thoại - hãy kiểm tra lại", "orange"),
                 }
                 display_name, icon = label_map_display.get(label, (label, "❓"))
-                text += f"<span style='color:{color}; font-weight:bold;'>{display_text}</span>"
+                text += f"{display_name}"
             result_placeholder_4.markdown(text)
 else:
     st.info("⬆️ Hãy tải một ảnh lên để cả bốn mô hình cùng phân tích.")
